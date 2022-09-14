aedad5c73330524009da09de7652aca71ed7c41d
kaj117 <70688258+kaj117@users.noreply.github.com>
2022-09-14 12:23:23 +0530

    Update TldrawWebviewManager.ts

--- apps/vscode/extension/src/TldrawWebviewManager.ts
+++ apps/vscode/extension/src/TldrawWebviewManager.ts
@@ -159,7 +159,7 @@ export class TldrawWebviewManager {
           <meta charset="utf-8" />
           <link rel="stylesheet" href="${cssSrc}" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>tldraw</title>
+          <title>Draw Pad</title>
         </head>
         <body>
           <div id="root"></div>
